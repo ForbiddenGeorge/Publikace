f718ef41a3ab69fabacdb41a140c3622d00bd614
Kavic15 <kavatomas15@seznam.cz>
2023-06-19 10:01:25 +0200

    Přidání Alertů

--- src/components/EditCardNavBarEditInfo.js
+++ src/components/EditCardNavBarEditInfo.js
@@ -2,50 +2,53 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 // import { SaveEditedPublication } from "./SaveEditedPublicationButton";
 import { PublicationUpdateMutation } from "querries/PublicationUpdateMutation";
-//import { type } from "@testing-library/user-event/dist/type";
-//tady to je mutace kterou budeš volat na kliknutí buttonu, zatím není zprovozněná
-//import { PublicationUpdateMutation } from "querries/PublicationUpdateMutation";
+import AlertPositive from "components/AlertPositive";
+import AlertNegative from "components/AlertNegative";
 
 function EditCardNavBarEditInfo({ publicationId }) {
     const publications = useSelector((state) => state.publications); //pole všech publikací, z tohoto pole pak vyberu naši správnou publikaci
     const publicationTypes = useSelector((state) => state.publicationTypes) //Typy těch publikací
     const [publicationType, setPublicationType] = useState('');//Uložený vybraný typ publikace
-    const selectedPublication = publications.find((publication) => publication.id === publicationId.publicationId); //tady je ta konkrétní publikace se kterou pracujeme
-    //console.log("Ta moje publikace ", selectedPublication);
+    const selectedPublication = publications.find((publication) => publication.id === publicationId.publicationId); // konkrétní publikace se kterou pracujeme
     const [name, setName] = useState(''); // proměnná pro ukládání názvu
     const [place, setPlace] = useState(''); // proměnná pro ukládání místa
     const [reference, setReference] = useState(''); // proměnná pro ukládání reference
 
-    const handlePublicationUpdate = () =>{
-      
-      // OŠETŘENÍ PRÁZDNÝCH MÍST
-      // Pokud se při editaci (stisku tlačítka) nezmění jistá hodnota
-      const updatedName = name || selectedPublication.name;
-      const updatedType = publicationType || selectedPublication.publicationtype.id;
-      const updatedReference = reference || selectedPublication.reference;
-      const updatedPlace = place || selectedPublication.place;
-      
-      //VOLÁNÍ MUTACE Původní verze
-      // PublicationUpdateMutation({ 
-      //   pubId:selectedPublication.id,
-      //   pubName:name,
-      //   pubLastChange: selectedPublication.lastchange,
-      //   pubTypeId: publicationType,
-      //   pubReference: reference,
-      //   pubPlace: place
-      // })
+    const [showAlert, setShowAlert] = useState(false);
+    const [showAlert1, setShowAlert1] = useState(false);
+    const handleCloseAlert = () => {
+          setShowAlert(false);
+          setShowAlert1(false);
+    };
 
-      // VOLÁNÍ MUTACE
-      PublicationUpdateMutation({ 
-        pubId:selectedPublication.id,
-        pubName:updatedName,
-        pubLastChange: selectedPublication.lastchange,
-        pubTypeId: updatedType,
-        pubReference: updatedReference,
-        pubPlace: updatedPlace
-      })
+    // Funkce při stisku tlačítka
+    const handlePublicationUpdate = async () =>{
+      try{
+        // OŠETŘENÍ PRÁZDNÝCH MÍST
+        // Pokud se při editaci (stisku tlačítka) nezmění jistá hodnota
+        const updatedName = name || selectedPublication.name;
+        const updatedType = publicationType || selectedPublication.publicationtype.id;
+        const updatedReference = reference || selectedPublication.reference;
+        const updatedPlace = place || selectedPublication.place;
+
+        // VOLÁNÍ MUTACE
+        PublicationUpdateMutation({ 
+          pubId:selectedPublication.id,
+          pubName:updatedName,
+          pubLastChange: selectedPublication.lastchange,
+          pubTypeId: updatedType,
+          pubReference: updatedReference,
+          pubPlace: updatedPlace
+        })
+
+        console.log('Publication edited successfully!');
+        setShowAlert(true);
+      }
+      catch (error) {
+        console.error('Error editing publication:', error);
+        setShowAlert1(true);
+      }
     }
-   
     
     return (
         <div className="container">
@@ -72,6 +75,8 @@ function EditCardNavBarEditInfo({ publicationId }) {
               ))}
             </select>            
             <button type="button" className="btn bg-success text-white" onClick={handlePublicationUpdate}>Uložit</button>
+            {showAlert && <AlertPositive info={"Publikace úspěšně upravena"} onClose={handleCloseAlert} />}
+            {showAlert1 && <AlertNegative info={"Nepovedlo upravit publikaci"} onClose={handleCloseAlert} />}
         </div>
     );
 }
