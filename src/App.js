3f02266530ac67978c659f4eceeec23357c18b1f
ForbiddenGeorge <j.holik@gymzn.cz>
2023-07-13 19:56:15 +0200

    fix app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PublicationTypeLoad from 'components/PublicationTypeLoad';
 import TopHeader from 'components/TopHeader';
+import PublicationLoad from 'components/PublicationLoad';
 import Filter from 'components/Filter';
 
 //Volání základních komponent
